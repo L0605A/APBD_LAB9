025f7719a6834da0fa1a4a377031d177f6ce6b35
s27888 <l0605@pjwstk.edu.pl>
2024-05-28 15:02:32 +0200

    Something sometihing dont care I aint payed enought, Matter of fact I dont get payed at all

---
+++ APBD9/APBD9/Controllers/TripsController.cs
@@ -0,0 +1,35 @@
+﻿using APBD9.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD9.Controllers;
+
+[ApiController]
+[Route("/api/trips")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+
+        var trips = await _context.Trips
+            .Select(e => new
+            {
+                Name = e.Name,
+                Countries = e.IdCountries.Select(c => new
+                {
+                    Name = c.Name
+                })
+            }).ToListAsync();
+        
+        return Ok(trips);
+    }
+}
